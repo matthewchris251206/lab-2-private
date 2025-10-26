675767ad32cecf34fe89ad5231dd1676e36fc7e7
matthewchris251206 <matthewchrislau@gmail.com>
2025-10-26 12:32:43 +0800

    Add files via upload

---
+++ build/iss/src/CMakeFiles/iss.dir/cmake_clean.cmake
@@ -0,0 +1,29 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/iss.dir/abstract_mem.c.o"
+  "CMakeFiles/iss.dir/abstract_mem.c.o.d"
+  "CMakeFiles/iss.dir/core.c.o"
+  "CMakeFiles/iss.dir/core.c.o.d"
+  "CMakeFiles/iss.dir/halt.c.o"
+  "CMakeFiles/iss.dir/halt.c.o.d"
+  "CMakeFiles/iss.dir/iss.c.o"
+  "CMakeFiles/iss.dir/iss.c.o.d"
+  "CMakeFiles/iss.dir/load_elf.c.o"
+  "CMakeFiles/iss.dir/load_elf.c.o.d"
+  "CMakeFiles/iss.dir/main_mem.c.o"
+  "CMakeFiles/iss.dir/main_mem.c.o.d"
+  "CMakeFiles/iss.dir/mem_map.c.o"
+  "CMakeFiles/iss.dir/mem_map.c.o.d"
+  "CMakeFiles/iss.dir/rom.c.o"
+  "CMakeFiles/iss.dir/rom.c.o.d"
+  "CMakeFiles/iss.dir/text_buffer.c.o"
+  "CMakeFiles/iss.dir/text_buffer.c.o.d"
+  "CMakeFiles/iss.dir/tick.c.o"
+  "CMakeFiles/iss.dir/tick.c.o.d"
+  "libiss.a"
+  "libiss.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/iss.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
